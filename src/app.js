689cad3f0a37671115b0722e4b20d3244fbbadbf
S0MBRA99 <lyonelworks99@gmail.com>
2025-08-20 17:52:46 +0200

    structure did it

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
-import "bootstrap";
-import "./style.css";
+function randomNumberOrLetter(){
 
+    let randomNumber = Math.floor((Math.random()*12)+1)
+    
+    if(randomNumber > 1 && randomNumber < 10){
+        return randomNumber
+    }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    else{
+        if(randomNumber === 1){
+            randomNumber = 'A' 
+        }
+        else if(randomNumber === 10){
+            randomNumber =  'J'
+        }
+        else if(randomNumber === 11){
+            randomNumber =  'Q'
+        }
+        else{
+            randomNumber = 'K'
+        }
+        return randomNumber
+    }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomEmoji(){
+    let randomNumberEmoji = Math.floor(Math.random()*4)
+     
+    if(randomNumberEmoji === 0){
+        return '♦'
+    }
+    else if(randomNumberEmoji === 1){
+        return '♥'
+    }
+    else if(randomNumberEmoji === 2){
+        return '♠'
+    }
+    else{
+        return '♣'
+    }    
+}
+
+window.onload = ()=>{
+    let headerCard = document.querySelector('#headerCard')
+    let bodyCard = document.querySelector('#bodyCard')
+    let footerCard = document.querySelector('#footerCard')
+
+    let numberLetterResult = randomNumberOrLetter()
+    let emojiResult = randomEmoji() 
+    
+    headerCard.textContent = emojiResult 
+    bodyCard.textContent = numberLetterResult
+    footerCard.textContent = emojiResult
+}
